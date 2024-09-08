9a8b6e5909cc427e7f36e923c2732def61c51dd0
FarazAhmed1234 <SP24BSCS0044@maju.edu.pk>
2024-09-08 23:26:03 +0500

    Add files via upload

---
+++ Binary search.cpp
@@ -0,0 +1,43 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num, first = 0,last = 14,mid;
+//     int a[15]={2,3,6,10,20,22,34,56,57,58,76,87,98,99,123};
+
+//     cout<<"Enter the number find the location: "<<endl;
+//     cin>>num;
+//      do
+//      {
+//         mid=(first+last)/2;
+//         if (a[mid]==num)
+//         {
+//             cout<<"Found at location = "<<mid+1<<endl;
+//             break;
+//         }
+//         else if (a[mid]<num)
+        
+//             first = mid+1;
+        
+//         else
+        
+//             last = mid-1;
+        
+//      } while (first<=last);
+     
+//      if(first>last){
+//         cout<<"Number not found."<<endl;
+//      }
+// return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
