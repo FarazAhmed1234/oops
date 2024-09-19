87e346aeb0c4e52255c627dd61c62937e127c41b
FarazAhmed1234 <SP24BSCS0044@maju.edu.pk>
2024-09-19 19:52:25 +0500

    Add files via upload
    Take input from user size and array elements and find the reverse order in an array.

--- Reverse_Order.cpp
+++ Reverse_Order.cpp
@@ -1,38 +1,28 @@
+    
+#include <iostream>
+using namespace std;
 
-
- #include <iostream>
- using namespace std;
-
- int main()
-{
-
+int main() {
     int size;
-    cout << "Enter the size is array is  :";
+
+    // Taking array size input from the user
+    cout << "Enter the size of the array: ";
     cin >> size;
 
-    int *array = new int[size];
+    int arr[size];
 
-    for (int i = 0; i < size; i++)
-    {
-        cout << "Enter the " << i + 1 << " Elenemts is : ";
-
-        cin >> array[i];
+    // Taking array elements input from the user
+    cout << "Enter " << size << " elements for the array:\n";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
     }
 
-    cout << "The array in order is: "<<endl;
-    for (int i = 0; i < size; i++)
-{
-        cout<<array[i]<<" ";
-}
-cout<<endl;
+    // Output the array elements in reverse order
+    cout << "The array in reverse order is: ";
+    for (int i = size - 1; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
-
-    cout << "The array in reverse order is: "<<endl;
-for(int i = size-1; i>=0 ; i--){
-    cout<<array[i]<<" ";
-}
-cout<<endl;
-
- 
     return 0;
 }
